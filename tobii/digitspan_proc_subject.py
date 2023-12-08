753bdad58d853be64624a843a30614be3517daea
jelman <jelman523@gmail.com>
2023-12-08 15:54:27 -0800

    First running draft for VETSA data

--- tobii/digitspan_proc_subject.py
+++ tobii/digitspan_proc_subject.py
@@ -49,17 +49,17 @@ def plot_trials(pupildf, pupil_fname):
 def clean_trials(trialevents):
     resampled_dict = {}
     for trial in trialevents.Trial.unique():
-        starttime, stoptime =  trialevents.loc[trialevents.Trial==trial,'TETTime'].iloc[[0,-1]]
-        rawtrial = trialevents.loc[(trialevents.TETTime>=starttime) & (trialevents.TETTime<=stoptime)]
+        starttime, stoptime =  trialevents.loc[trialevents.Trial==trial,'RTTime'].iloc[[0,-1]]
+        rawtrial = trialevents.loc[(trialevents.RTTime>=starttime) & (trialevents.RTTime<=stoptime)]
         cleantrial = pupil_utils.deblink(rawtrial)
-        string_cols = ['Load', 'Trial', 'Condition']
+        string_cols = ['Load', 'Trial', 'TrialId', 'Condition']
         trial_resamp = pupil_utils.resamp_filt_data(cleantrial, filt_type='low', string_cols=string_cols)
-        baseline = trial_resamp.loc[trial_resamp.Condition=='Ready', 'DiameterPupilLRFilt'].last('250ms').mean()
+        baseline = trial_resamp.loc[trial_resamp.Condition=='Ready', 'PupilDiameterLRFilt'].last('250ms').mean()
         baseline_blinks = trial_resamp.loc[trial_resamp.Condition=='Ready', 'BlinksLR'].last('250ms').mean()
         if baseline_blinks > .5:
             baseline = np.nan
         trial_resamp['Baseline'] = baseline
-        trial_resamp['Dilation'] = trial_resamp['DiameterPupilLRFilt'] - trial_resamp['Baseline']
+        trial_resamp['Dilation'] = trial_resamp['PupilDiameterLRFilt'] - trial_resamp['Baseline']
         trial_resamp = trial_resamp[trial_resamp.Condition=='Record']
         trial_resamp.index = pd.DatetimeIndex((trial_resamp.index - trial_resamp.index[0]).astype(np.int64))
         resampled_dict[trial] = trial_resamp        
@@ -67,36 +67,43 @@ def clean_trials(trialevents):
     return dfresamp
     
 def define_condition(trialdf):
-    trialdf['Condition'] = np.where((trialdf.TETTime - trialdf.TETTime.iloc[0]) / 1000. > 1., 'Record', 'Ready')
+    trialdf['Condition'] = np.where((trialdf.RTTime - trialdf.RTTime.iloc[0]) / 1000. > 1., 'Record', 'Ready')
     return trialdf
 
 
 def get_trial_events(df):
     """
     Create dataframe of trial events. This includes:
-        Load: [3, 4, 5, 6, 7, 8, 9] Number of digits to recall
+        Load: [Baseline, 3, 6, 9] Number of digits to recall
         Trial: Lists load and trial number within each load
         Condition: ['Ready', 'Record'] Phase of trial
     First finds timestamps where each load trial begins and ends. Appends info
     about the load and trial number. Also defines phase of each trial. 
     """
-    loads = [x for x in df.CurrentObject.unique() if "Recall" in str(x)]
+    trials = [x for x in df.Trial.unique()]
     # Find last index of each load
-    lastidx = [df[df.CurrentObject==x].index[-1] for x in loads]
+    lastidx = [df[df.Trial==x].index[-1] for x in trials]
     stopidx = [x + 1 for x in lastidx]
     startidx = stopidx[:-1]
     startidx.insert(0,0)
     triallist = []
-    for i, cond in enumerate(loads):
+    for i, cond in enumerate(trials):
         loaddf = df[startidx[i]:stopidx[i]]
         loaddf = loaddf[loaddf['CurrentObject']=='Ready']
-        loaddf['Load'] = cond.replace('RecallDS','')
-        loaddf['Trial'] = cond + '_' + loaddf['TrialId'].astype(str)
+        # Confirm TrialId is A, B, or C
+        assert all(x in ['A','B','C'] for x in loaddf['TrialId'].unique())        
         loaddf = loaddf.groupby('Trial').apply(define_condition)
         triallist.append(loaddf)
     trialevents = pd.concat(triallist)    
     return trialevents
 
+
+def recode_digitlist(digitlist):
+    """Recode DigitList values to """
+    digitlist = digitlist.str.replace('ListLoad','')
+    digitlist = digitlist.str.replace('1','A')
+    digitlist = digitlist.str.replace('2','B')
+    return digitlist
    
 def proc_subject(filelist, outdir):
     """Given an infile of raw pupil data, saves out:
@@ -106,13 +113,24 @@ def proc_subject(filelist, outdir):
         4. Percent of samples with blinks """
     for fname in filelist: 
         print('Processing {}'.format(fname))
-        if (fname.lower().endswith(".gazedata")) | (fname.lower().endswith(".csv") | (fname.lower().endswith(".txt")):
+        if fname.lower().endswith(".gazedata") | fname.lower().endswith(".csv") | fname.lower().endswith(".txt"):
             df = pd.read_csv(fname, sep="\t")
-        elif (fname.lower().endswith(".xlsx")):
+        elif fname.lower().endswith(".xlsx"):
             df = pd.read_excel(fname)
         else: 
             raise IOError('Could not open {}'.format(fname))  
         subid = pupil_utils.get_vetsaid(df, fname)
+        df['Subject'] = subid
+        # Load column is incorrect, remove. It will be generated correctly from DigitList
+        df = df.drop('Load', axis=1)
+        # Recode DigitList values
+        df['Trial'] = recode_digitlist(df['DigitList'])
+        # Convert PupilDiameterLeftEye and PupilDiameterRightEye to numeric
+        df['PupilDiameterLeftEye'] = pd.to_numeric(df['PupilDiameterLeftEye'], errors='coerce')
+        df['PupilDiameterRightEye'] = pd.to_numeric(df['PupilDiameterRightEye'], errors='coerce')        # Create Load and TrialId columns
+        # Create Load and TrialId columns
+        df['Load'] = df['Trial'].str[:-1]
+        df['TrialId'] = df['Trial'].str[-1]
         trialevents = get_trial_events(df)
         dfresamp = clean_trials(trialevents)
         dfresamp = dfresamp.reset_index(level='Timestamp').set_index(['Load','Trial'])
@@ -121,18 +139,14 @@ def proc_subject(filelist, outdir):
         # pupildf.to_csv(pupil_outname, index=True)
         
         dfresamp1s = dfresamp.groupby(level=['Load','Trial']).apply(lambda x: x.resample('1s', on='Timestamp', closed='right', label='right').mean(numeric_only=True)).reset_index()
+        dfresamp1s['Subject'] = subid
         # Select and rename columns of interest
         pupilcols = ['Subject', 'Trial', 'Load', 'Timestamp', 'Dilation',
-                     'Baseline', 'DiameterPupilLRFilt', 'BlinksLR']
-        dfresamp1s = dfresamp1s[pupilcols].rename(columns={'DiameterPupilLRFilt':'Diameter',
+                     'Baseline', 'PupilDiameterLRFilt', 'BlinksLR']
+        dfresamp1s = dfresamp1s[pupilcols].rename(columns={'PupilDiameterLRFilt':'Diameter',
                                                  'BlinksLR':'BlinkPct'})
         # Set samples with >50% blinks to missing    
         dfresamp1s.loc[dfresamp1s.BlinkPct>.5, ['Dilation','Baseline','Diameter','BlinkPct']] = np.nan
-        # Convert Load to int
-        dfresamp1s['Load'] = dfresamp1s['Load'].astype(int)
-        # Take average of each second
-        dfresamp1s = dfresamp1s.groupby('Trial').filter(lambda x: x['Timestamp'].max().timestamp() <= (x['Load'].iloc[0] + 1))
-
         # Drop missing samples and average of trials within load
         pupildf = dfresamp1s.groupby(['Load','Timestamp']).mean(numeric_only=True)
         # Set subject ID and session as (as type string)
@@ -141,7 +155,7 @@ def proc_subject(filelist, outdir):
         pupildf['ntrials'] = dfresamp1s.dropna(subset=['Dilation']).groupby(['Load','Timestamp']).size()
         pupildf = pupildf.reset_index()
         pupildf['Timestamp'] = pupildf.Timestamp.dt.strftime('%H:%M:%S')
-        pupildf = pupildf[['Subject','Session','Load','Timestamp','Baseline','Diameter','Dilation','BlinkPct','ntrials']]
+        pupildf = pupildf[['Subject','Load','Timestamp','Baseline','Diameter','Dilation','BlinkPct','ntrials']]
         # Generate output filename
         pupil_outname = os.path.join(outdir, 'DigitSpan_' + subid + '_ProcessedPupil.csv')
         print('Writing processed data to {0}'.format(pupil_outname))
